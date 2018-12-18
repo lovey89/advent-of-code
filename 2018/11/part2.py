5218fe3a0b3ce38b798bfa7a76b915bafd4e3b03
Love Yregård <xlove.github@yregard.se>
2018-12-18 16:42:10 +0100

    refactor 2018 day11 part 2

--- 2018/11/part2.py
+++ 2018/11/part2.py
@@ -10,42 +10,24 @@ def power(x, y):
 
 # https://en.wikipedia.org/wiki/Summed-area_table
 def create_summed_area_table(power_grid):
-    summed_area_table = [[0 for y in range(grid_size)] for x in range(grid_size)]
-    for x in range(grid_size):
-        for y in range(grid_size):
+    summed_area_table = [[0 for y in range(grid_size + 1)] for x in range(grid_size + 1)]
+    for x in range(1, grid_size + 1):
+        for y in range(1, grid_size + 1):
             diag =  summed_area_table[x - 1][y - 1]
             left =  summed_area_table[x - 1][y]
             above = summed_area_table[x][y - 1]
-            if x == 0:
-                left = 0
-                diag = 0
-            if y == 0:
-                right = 0
-                diag = 0
 
-            summed_area_table[x][y] = left + above - diag + power_grid[x][y]
+            summed_area_table[x][y] = left + above - diag + power_grid[x - 1][y - 1]
 
     return summed_area_table
 
 def calculate_sum(left, top, s, summed_area_table):
     right = left + s - 1
     bottom = top + s - 1
-    if left == 0 and top == 0:
-        area_above = 0
-        area_left  = 0
-        area_diag  = 0
-    elif left == 0:
-        area_above = summed_area_table[right][top - 1]
-        area_left  = 0
-        area_diag  = 0
-    elif top == 0:
-        area_above = 0
-        area_left  = summed_area_table[left - 1][bottom]
-        area_diag  = 0
-    else:
-        area_above = summed_area_table[right][top - 1]
-        area_left  = summed_area_table[left - 1][bottom]
-        area_diag  = summed_area_table[left - 1][top - 1]
+
+    area_above = summed_area_table[right][top - 1]
+    area_left  = summed_area_table[left - 1][bottom]
+    area_diag  = summed_area_table[left - 1][top - 1]
 
     return summed_area_table[right][bottom] + area_diag - area_above - area_left
 
@@ -56,8 +38,8 @@ if __name__ == '__main__':
 
     max_power = 0
     for s in range(1, grid_size + 1):
-        for x in range(grid_size - s + 1):
-            for y in range(grid_size - s + 1):
+        for x in range(1, grid_size - s + 2):
+            for y in range(1, grid_size - s + 2):
                 total_power = calculate_sum(x, y, s, summed_area_table)
                 if total_power > max_power:
                     max_power = total_power
@@ -65,4 +47,4 @@ if __name__ == '__main__':
                     max_power_y = y
                     max_power_s = s
 
-    print(max_power, max_power_x + 1, max_power_y + 1, max_power_s) # The grid is off by one hence we need to add 1
+    print(max_power, max_power_x, max_power_y, max_power_s) # The grid is off by one hence we need to add 1

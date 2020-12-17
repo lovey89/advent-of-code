f5f61607935ab4a522f0079063f9a3868b21ded1
Love Yregård <xlove.github@yregard.se>
2020-12-18 00:56:52 +0100

    2020 day 17

---
+++ 2020/17/part2.js
@@ -0,0 +1,99 @@
+"use strict"
+
+let fs = require('fs');
+
+let input =
+  fs.readFileSync('input.txt').toString()
+    .split("\n")
+    .filter(Boolean)
+    .map(line => line.split(""));
+
+const generations = 6;
+const tMax = generations * 2 + 1;
+const zMax = generations * 2 + 1;
+const yMax = generations * 2 + input.length;
+const xMax = generations * 2 + input[0].length;
+
+let initialCube = createCube();
+
+for (let y = 0; y < input.length; y++) {
+  for (let x = 0; x < input[0].length; x++) {
+    initialCube[generations][generations][generations + y][generations + x] = input[y][x];
+  }
+}
+
+let cube = initialCube;
+for (let i = 0; i < generations; i++) {
+  cube = nextGen(cube);
+}
+
+console.log(countActive(cube));
+
+function countActive(cube) {
+  let count = 0;
+  for (let t = 0; t < tMax; t++) {
+    for (let z = 0; z < zMax; z++) {
+      for (let y = 0; y < yMax; y++) {
+        for (let x = 0; x < xMax; x++) {
+          if (cube[t][z][y][x] == '#') {
+            count++;
+          }
+        }
+      }
+    }
+  }
+  return count;
+}
+
+function nextGen(cube) {
+  let nextGenCube = createCube();
+
+  for (let t = 0; t < tMax; t++) {
+    for (let z = 0; z < zMax; z++) {
+      for (let y = 0; y < yMax; y++) {
+        for (let x = 0; x < xMax; x++) {
+          let count = neighbors(cube, x, y, z, t);
+          let char = cube[t][z][y][x];
+          if (char == '.' && count == 3) {
+            nextGenCube[t][z][y][x] = '#';
+          } else if (char == '#' && !(count == 3 || count == 2)) {
+            nextGenCube[t][z][y][x] = '.';
+          } else {
+            nextGenCube[t][z][y][x] = char;
+          }
+        }
+      }
+    }
+  }
+  return nextGenCube;
+}
+
+function neighbors(cube, xc, yc, zc, tc) {
+  let count = 0;
+  for (let t = Math.max(0, tc - 1); t < Math.min(tMax, tc + 2); t++) {
+    for (let z = Math.max(0, zc - 1); z < Math.min(zMax, zc + 2); z++) {
+      for (let y = Math.max(0, yc - 1); y < Math.min(yMax, yc + 2); y++) {
+        for (let x = Math.max(0, xc - 1); x < Math.min(xMax, xc + 2); x++) {
+          if (x == xc && y == yc && z == zc && t == tc) {
+            continue;
+          }
+          let char = cube[t][z][y][x];
+          if (char == '#') {
+            count++;
+          }
+        }
+      }
+    }
+  }
+  return count;
+}
+
+function createCube() {
+  return Array(tMax)
+    .fill(null)
+    .map(ignore => Array(zMax)
+      .fill(null)
+      .map(ignore => Array(yMax)
+        .fill(null)
+        .map(ignore2 => Array(xMax).fill("."))));
+}

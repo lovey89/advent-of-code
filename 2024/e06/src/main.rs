c50958e3690f717f34aa97660ee69c7bcfca9647
Love Yregård <xlove.github@yregard.se>
2024-12-09 20:16:28 +0100

    2024 day 6 part 2

--- 2024/e06/src/main.rs
+++ 2024/e06/src/main.rs
@@ -1,12 +1,11 @@
 use std::fs;
 
 const INPUT_FILE: &str = "input.txt";
-//const INPUT_FILE: &str = "mini.txt";
 
-#[derive(PartialEq)]
+#[derive(PartialEq, Clone, Copy)]
 enum Position {
     Obstacle,
-    Visited,
+    Visited(bool, bool, bool, bool), // up, down, left, right
     Unvisited,
 }
 
@@ -15,13 +14,80 @@ impl From<char> for Position {
         match value {
             '.' => Position::Unvisited,
             '#' => Position::Obstacle,
-            '^' => Position::Visited,
+            '^' => Position::Visited(true, false, false, false),
             _ => panic!("Should not appear in input"),
         }
     }
 }
 
-fn read_input(filename: &str) -> (Vec<Vec<Position>>, (i32, i32)) {
+impl Position {
+    fn is_visited_before(self, dir: &Direction) -> bool {
+        let (up, down, left, right) = match self {
+            Position::Visited(up, down, left, right) => (up, down, left, right),
+            _ => return false,
+        };
+        match dir {
+            Direction::Up => up,
+            Direction::Down => down,
+            Direction::Left => left,
+            Direction::Right => right,
+        }
+    }
+
+    fn visit_position(self, dir: &Direction) -> Position {
+        let (up, down, left, right) = match self {
+            Position::Visited(up, down, left, right) => (up, down, left, right),
+            _ => (false, false, false, false),
+        };
+        match dir {
+            Direction::Up => Position::Visited(true, down, left, right),
+            Direction::Down => Position::Visited(up, true, left, right),
+            Direction::Left => Position::Visited(up, down, true, right),
+            Direction::Right => Position::Visited(up, down, left, true),
+        }
+    }
+}
+
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+impl Direction {
+    fn turn(&self) -> Direction {
+        match self {
+            Direction::Up => Self::Right,
+            Direction::Down => Self::Left,
+            Direction::Left => Self::Up,
+            Direction::Right => Self::Down,
+        }
+    }
+
+    fn next_coordinate(&self, (y, x): (usize, usize)) -> Option<(usize, usize)> {
+        match self {
+            Direction::Up => {
+                if y == 0 {
+                    None
+                } else {
+                    Some((y - 1, x))
+                }
+            }
+            Direction::Down => Some((y + 1, x)),
+            Direction::Left => {
+                if x == 0 {
+                    None
+                } else {
+                    Some((y, x - 1))
+                }
+            }
+            Direction::Right => Some((y, x + 1)),
+        }
+    }
+}
+
+fn read_input(filename: &str) -> (Vec<Vec<Position>>, (usize, usize)) {
     let map: Vec<Vec<Position>> = fs::read_to_string(filename)
         .unwrap()
         .lines()
@@ -31,8 +97,8 @@ fn read_input(filename: &str) -> (Vec<Vec<Position>>, (i32, i32)) {
     let mut start_position = None;
     'outer: for (y, row) in map.iter().enumerate() {
         for (x, pos) in row.iter().enumerate() {
-            if *pos == Position::Visited {
-                start_position = Some((y.try_into().unwrap(), x.try_into().unwrap()));
+            if matches!(pos, Position::Visited(..)) {
+                start_position = Some((y, x));
                 break 'outer;
             }
         }
@@ -40,48 +106,92 @@ fn read_input(filename: &str) -> (Vec<Vec<Position>>, (i32, i32)) {
     (map, start_position.unwrap())
 }
 
-fn part_1() {
-    let (mut map, (mut current_y, mut current_x)) = read_input(INPUT_FILE);
-    let (mut y_dir, mut x_dir) = (-1, 0);
-    let height = map.len().try_into().unwrap();
-    let width = map[0].len().try_into().unwrap();
+fn get_visited_map(
+    map: &Vec<Vec<Position>>,
+    (mut current_y, mut current_x): (usize, usize),
+) -> Option<Vec<Vec<Position>>> {
+    let mut map = map.to_owned();
+    let mut dir = Direction::Up;
+    let height = map.len();
+    let width = map[0].len();
     loop {
-        let y_next = current_y + y_dir;
-        let x_next = current_x + x_dir;
-        if y_next < 0 || y_next >= height || x_next < 0 || x_next >= width {
+        let next_coord = dir.next_coordinate((current_y, current_x));
+        let Some((y_next, x_next)) = next_coord else {
+            break;
+        };
+        if y_next >= height || x_next >= width {
             break;
         }
-        if map[TryInto::<usize>::try_into(y_next).unwrap()]
-            [TryInto::<usize>::try_into(x_next).unwrap()]
-            == Position::Obstacle
-        {
-            if x_dir != 0 {
-                (y_dir, x_dir) = (x_dir, 0);
-            } else {
-                (y_dir, x_dir) = (0, -y_dir);
-            }
+        let current_position = map[y_next][x_next];
+        if current_position == Position::Obstacle {
+            dir = dir.turn();
             continue;
         }
-        map[TryInto::<usize>::try_into(y_next).unwrap()]
-            [TryInto::<usize>::try_into(x_next).unwrap()] = Position::Visited;
+        if current_position.is_visited_before(&dir) {
+            return None;
+        }
+        map[y_next][x_next] = current_position.visit_position(&dir);
         current_y = y_next;
         current_x = x_next;
     }
-    let res: i32 = map
+    Some(map)
+}
+
+fn part_1() {
+    let (map, (current_y, current_x)) = read_input(INPUT_FILE);
+    let visited_map = get_visited_map(&map, (current_y, current_x))
+        .expect("The unaltered map should not contain any loops");
+    let res: i32 = visited_map
         .into_iter()
         .map(|line| {
-            TryInto::<i32>::try_into(line.into_iter().filter(|p| *p == Position::Visited).count())
-                .unwrap()
+            TryInto::<i32>::try_into(
+                line.into_iter()
+                    .filter(|p| matches!(p, Position::Visited(..)))
+                    .count(),
+            )
+            .unwrap()
         })
         .sum();
     println!("{res}");
 }
 
 fn part_2() {
-    todo!()
+    let (mut map, (current_y, current_x)) = read_input(INPUT_FILE);
+    let visited_map = get_visited_map(&map, (current_y, current_x))
+        .expect("The unaltered map should not contain any loops");
+
+    let mut visited_positions: Vec<(usize, usize)> = vec![];
+    for (y, row) in visited_map.iter().enumerate() {
+        for (x, coord) in row.iter().enumerate() {
+            if matches!(coord, Position::Visited(..)) {
+                visited_positions.push((y, x));
+            }
+        }
+    }
+    let res = visited_positions
+        .into_iter()
+        .filter(|(y, x)| {
+            //let mut modified_map = map.clone();
+            map[*y][*x] = Position::Obstacle;
+            let visited_map = get_visited_map(&map, (current_y, current_x));
+            map[*y][*x] = Position::Unvisited;
+            visited_map.is_none()
+        })
+        .count();
+    println!("{res}");
 }
 
 fn main() {
+    use std::time::Instant;
+    println!("Day 6");
+    println!("---------------");
+    let now = Instant::now();
     part_1();
+    let elapsed = now.elapsed();
+    println!("part1 : {:.2?}", elapsed);
+    let now = Instant::now();
     part_2();
+    let elapsed = now.elapsed();
+    println!("part2 : {:.2?}", elapsed);
+    println!("---------------");
 }

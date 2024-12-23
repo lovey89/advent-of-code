97840993e7bb71d823c9c90c6a3ad27bf6d6ea8b
Love Yregård <xlove.github@yregard.se>
2024-12-23 12:13:31 +0100

    2024 day 15 part 2

--- 2024/e15/src/main.rs
+++ 2024/e15/src/main.rs
@@ -1,12 +1,14 @@
 use std::fs;
 
+use itertools::Itertools;
+
 const INPUT_FILE: &str = "input.txt";
-//const INPUT_FILE: &str = "mini.txt";
 
 #[derive(Debug, Clone, Copy, PartialEq)]
 enum Tile {
     Wall,
-    Box,
+    LeftBox,
+    RightBox,
     Empty,
 }
 
@@ -14,14 +16,14 @@ impl From<char> for Tile {
     fn from(value: char) -> Self {
         match value {
             '#' => Tile::Wall,
-            'O' => Tile::Box,
+            'O' => Tile::LeftBox,
             '.' | '@' => Tile::Empty,
             _ => panic!(),
         }
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 enum MoveDirection {
     Up,
     Down,
@@ -44,6 +46,10 @@ impl TryFrom<char> for MoveDirection {
 }
 
 impl MoveDirection {
+    fn is_vertical(&self) -> bool {
+        *self == MoveDirection::Up || *self == MoveDirection::Down
+    }
+
     fn update_map(&self, map: &mut [Vec<Tile>], (y, x): (usize, usize)) -> (usize, usize) {
         let (candidate_y, candidate_x) = self.new_coord((y, x));
 
@@ -60,7 +66,7 @@ impl MoveDirection {
             if tile == Tile::Wall {
                 return (y, x);
             } else if tile == Tile::Empty {
-                map[ty][tx] = Tile::Box;
+                map[ty][tx] = Tile::LeftBox;
                 map[candidate_y][candidate_x] = Tile::Empty;
                 return (candidate_y, candidate_x);
             }
@@ -75,6 +81,52 @@ impl MoveDirection {
             MoveDirection::Left => (y, x - 1),
             MoveDirection::Right => (y, x + 1),
         }
+    }
+
+    fn prepare_update(
+        &self,
+        map: &[Vec<Tile>],
+        (y, x): (usize, usize),
+    ) -> Option<Vec<(usize, usize)>> {
+        let tile = map[y][x];
+        if tile == Tile::Empty {
+            return Some(vec![]);
+        } else if tile == Tile::Wall {
+            return None;
+        }
+        if self.is_vertical() {
+            if tile == Tile::LeftBox {
+                let mut res = vec![(y, x), (y, x + 1)];
+                res.append(&mut self.prepare_update(map, self.new_coord((y, x)))?);
+                res.append(&mut self.prepare_update(map, self.new_coord((y, x + 1)))?);
+                Some(res)
+            } else {
+                let mut res = vec![(y, x - 1), (y, x)];
+                res.append(&mut self.prepare_update(map, self.new_coord((y, x - 1)))?);
+                res.append(&mut self.prepare_update(map, self.new_coord((y, x)))?);
+                Some(res)
+            }
+        } else {
+            let mut res = vec![(y, x)];
+            res.append(&mut self.prepare_update(map, self.new_coord((y, x)))?);
+            Some(res)
+        }
+    }
+
+    fn update_map2(&self, map: &mut [Vec<Tile>], (y, x): (usize, usize)) -> (usize, usize) {
+        let (candidate_y, candidate_x) = self.new_coord((y, x));
+        let Some(updates) = self.prepare_update(map, (candidate_y, candidate_x)) else {
+            return (y, x);
+        };
+
+        // This could be faster. Instead of using unique from itertools we could keep track of which
+        // has already been handled using a hashset
+        for (y_0, x_0) in updates.into_iter().unique().rev() {
+            let (y_1, x_1) = self.new_coord((y_0, x_0));
+            map[y_1][x_1] = map[y_0][x_0];
+            map[y_0][x_0] = Tile::Empty;
+        }
+        (candidate_y, candidate_x)
     }
 }
 
@@ -110,6 +162,24 @@ fn read_input(filename: &str) -> (Vec<Vec<Tile>>, Vec<MoveDirection>, (usize, us
     (map, movements, start_position)
 }
 
+fn print_map(map: &[Vec<Tile>], (r_y, r_x): (usize, usize)) {
+    for (y, row) in map.iter().enumerate() {
+        for (x, t) in row.iter().enumerate() {
+            if y == r_y && x == r_x {
+                print!("@");
+                continue;
+            }
+            match t {
+                Tile::Wall => print!("#"),
+                Tile::LeftBox => print!("["),
+                Tile::RightBox => print!("]"),
+                Tile::Empty => print!("."),
+            }
+        }
+        println!()
+    }
+}
+
 fn part_1() {
     let (mut map, movements, (mut y, mut x)) = read_input(INPUT_FILE);
     for movement in movements {
@@ -121,7 +191,7 @@ fn part_1() {
         .map(|(y, line)| {
             line.into_iter()
                 .enumerate()
-                .filter(|(_, tile)| *tile == Tile::Box)
+                .filter(|(_, tile)| *tile == Tile::LeftBox)
                 .map(|(x, _)| y * 100 + x)
                 .sum::<usize>()
         })
@@ -130,7 +200,36 @@ fn part_1() {
 }
 
 fn part_2() {
-    todo!()
+    let (map, movements, (mut y, mut x)) = read_input(INPUT_FILE);
+    let mut map: Vec<Vec<Tile>> = map
+        .into_iter()
+        .map(|row| {
+            row.into_iter()
+                .flat_map(|t| match t {
+                    Tile::Wall => vec![Tile::Wall, Tile::Wall],
+                    Tile::LeftBox => vec![Tile::LeftBox, Tile::RightBox],
+                    Tile::Empty => vec![Tile::Empty, Tile::Empty],
+                    Tile::RightBox => panic!("Should not occur"),
+                })
+                .collect()
+        })
+        .collect();
+    x *= 2;
+    for movement in movements {
+        (y, x) = movement.update_map2(&mut map, (y, x));
+    }
+    let res = map
+        .into_iter()
+        .enumerate()
+        .map(|(y, line)| {
+            line.into_iter()
+                .enumerate()
+                .filter(|(_, tile)| *tile == Tile::LeftBox)
+                .map(|(x, _)| y * 100 + x)
+                .sum::<usize>()
+        })
+        .sum::<usize>();
+    println!("{res}");
 }
 
 fn main() {

bb62d11795b3a915e9dddbcb905b005ecddc0243
Love Yregård <xlove.github@yregard.se>
2024-12-27 16:55:06 +0100

    2024 day 16 part 1

--- 2024/e16/src/main.rs
+++ 2024/e16/src/main.rs
@@ -1,6 +1,12 @@
+use std::cmp::Ordering;
+use std::collections::{
+    BinaryHeap,
+    HashSet,
+};
 use std::fs;
 
 const INPUT_FILE: &str = "input.txt";
+//const INPUT_FILE: &str = "mini.txt";
 
 #[derive(Debug, Clone, Copy, PartialEq)]
 enum Tile {
@@ -20,7 +26,7 @@ impl From<char> for Tile {
     }
 }
 
-#[derive(Debug, Clone, Copy, PartialEq)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
 enum Direction {
     North,
     South,
@@ -28,9 +34,57 @@ enum Direction {
     East,
 }
 
+impl Direction {
+    fn next_coord(&self, (y, x): (usize, usize)) -> (usize, usize) {
+        match self {
+            Direction::North => (y - 1, x),
+            Direction::South => (y + 1, x),
+            Direction::West => (y, x - 1),
+            Direction::East => (y, x + 1),
+        }
+    }
+    fn any_sides_is_open(&self, (y, x): (usize, usize), map: &[Vec<Tile>]) -> bool {
+        match self {
+            Direction::North | Direction::South => {
+                map[y][x - 1] != Tile::Wall || map[y][x + 1] != Tile::Wall
+            }
+            Direction::West | Direction::East => {
+                map[y - 1][x] != Tile::Wall || map[y + 1][x] != Tile::Wall
+            }
+        }
+    }
+}
+
+#[derive(Copy, Clone, Eq, PartialEq)]
+struct State {
+    cost: usize,
+    position: (usize, usize),
+    direction: Direction,
+}
+
+impl Ord for State {
+    fn cmp(&self, other: &Self) -> Ordering {
+        // Notice that we flip the ordering on costs.
+        // In case of a tie we compare positions - this step is necessary
+        // to make implementations of `PartialEq` and `Ord` consistent.
+        other
+            .cost
+            .cmp(&self.cost)
+            .then_with(|| self.position.cmp(&other.position))
+            .then_with(|| self.direction.cmp(&other.direction))
+    }
+}
+
+impl PartialOrd for State {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
 fn read_input(filename: &str) -> (Vec<Vec<Tile>>, (usize, usize), Direction) {
     let mut start_pos = (0, 0);
-    let map = fs::read_to_string(filename).unwrap()
+    let map = fs::read_to_string(filename)
+        .unwrap()
         .lines()
         .enumerate()
         .map(|(y, line)| {
@@ -38,7 +92,7 @@ fn read_input(filename: &str) -> (Vec<Vec<Tile>>, (usize, usize), Direction) {
                 .enumerate()
                 .map(|(x, c)| {
                     if c == 'S' {
-                        start_pos = (y,x);
+                        start_pos = (y, x);
                     }
                     c.into()
                 })
@@ -49,7 +103,60 @@ fn read_input(filename: &str) -> (Vec<Vec<Tile>>, (usize, usize), Direction) {
 }
 
 fn part_1() {
-    todo!();
+    let (map, start_position, start_direction) = read_input(INPUT_FILE);
+
+    let mut heap = BinaryHeap::new();
+
+    heap.push(State {
+        cost: 0,
+        position: start_position,
+        direction: start_direction,
+    });
+    let mut visited_positions = HashSet::new();
+
+    while let Some(State {
+        cost,
+        position: (y, x),
+        direction,
+    }) = heap.pop()
+    {
+        if !visited_positions.insert((y, x)) {
+            // The set already contained this value
+            continue;
+        }
+        if map[y][x] == Tile::End {
+            println!("{}", cost);
+            return;
+        }
+        // Find neighbors
+        for dir in [
+            Direction::North,
+            Direction::South,
+            Direction::West,
+            Direction::East,
+        ] {
+            let mut last_pos = (y, x);
+            let mut additional_cost = if dir == direction { 1 } else { 1001 };
+            loop {
+                let (y_new, x_new) = dir.next_coord(last_pos);
+                let tile = map[y_new][x_new];
+                if tile == Tile::Wall {
+                    // We hit a wall
+                    break;
+                }
+                if tile == Tile::End || dir.any_sides_is_open((y_new, x_new), &map) {
+                    heap.push(State {
+                        cost: cost + additional_cost,
+                        position: (y_new, x_new),
+                        direction: dir,
+                    });
+                    break;
+                }
+                last_pos = (y_new, x_new);
+                additional_cost += 1;
+            }
+        }
+    }
 }
 
 fn part_2() {
